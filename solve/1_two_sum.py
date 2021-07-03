a111e6788a411ef489afdc9f0457462896c78c19
WonTaek <time_to_bye@naver.com>
2021-07-03 16:00:12 +0900

    add solutions of two_sum problem

--- solve/1_two_sum.py
+++ solve/1_two_sum.py
@@ -5,7 +5,6 @@
 """
 
 
-
 class Solution:
     def twoSum(self, numbers, target_number):
         for x_index, number in enumerate(numbers):
@@ -30,7 +29,58 @@ if __name__ == "__main__":
 
     print(result)
 
+# 위의 코드를 수정
+# O(n^2) 이 나오기는 하지만 in 연산 쪽이 훨씬 더 가볍고 빠름
+class Solution:
+    def twoSum(self, numbers, target_number):
+        for x_index, number in enumerate(numbers):
+            y = target_number - number
 
+            if y in numbers[x_index + 1:]:
+                return [x_index, numbers[x_index + 1:].index(y) + 1 + x_index]
+
+
+
+class Solution:
+    """
+    중복 되는 값이 있더라도, 앞에서 인덱스가 처리되기 때문에 문제 없음
+    """
+    def twoSum(self, numbers, target_number):
+        nums_dict = {}
+        for x_index, number in enumerate(numbers):
+            nums_dict[number] = x_index
+
+        # print(nums_dict)
+        for x_index, number in enumerate(numbers):
+            value = target_number - number
+
+            # if value in nums_dict and x_index != nums_dict[value]:
+            #     return [x_index, nums_dict[value]]
+
+            if nums_dict.get(value) and x_index != nums_dict[value]:
+                return [x_index, nums_dict[value]]
+
+    def twoSum_restructure(self, numbers, target_number):
+        nums_dict = {}
+        for x_index, number in enumerate(numbers):
+            value = target_number - number
+
+            # if value in nums_dict and x_index != nums_dict[value]:
+            #     return [x_index, nums_dict[value]]
+
+            if value in nums_dict:
+                return [nums_dict[value], x_index]
+            nums_dict[number] = x_index
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    Given_nums = [3, 3]
+    target = 6
+
+    result = solution.twoSum_restructure(Given_nums, target)
+
+    print(result)
 
 
 """legacy code
