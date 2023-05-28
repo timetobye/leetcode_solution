e5ef09db16ca3feb3d5409957f59349a02bab505
wonny <time_to_bye@naver.com>
2023-05-28 23:18:34 +0900

    add README.md

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ leetcode
 ----
 
 Algorithms...
+
+
+|문항번호|문제 이름|풀이 기록|성공 여부|비고|
+|---|---|---|---|---|
+|1|two_sum|[two_sum](https://github.com/timetobye/leetcode_solution/tree/master/solve/1_two_sum.py)|성공| |
+|3|Longest_Substring_Without_Repeating_Characters|[Longest_Substring_Without_Repeating_Characters](https://github.com/timetobye/leetcode_solution/tree/master/solve/3_Longest Substring Without Repeating Characters.py)|성공| |
+|5|Longest_Palindromic_Substring|[Longest_Palindromic_Substring](https://github.com/timetobye/leetcode_solution/tree/master/solve/5_Longest Palindromic Substring.py)|성공| |
+|7|Reverse_Integer|[Reverse_Integer](https://github.com/timetobye/leetcode_solution/tree/master/solve/7_Reverse_Integer.py)|성공| |
+|9|Palindrome_Number|[Palindrome_Number](https://github.com/timetobye/leetcode_solution/tree/master/solve/9_Palindrome_Number.py)|성공| |
--- solve/test.py
+++ solve/test.py
@@ -1,4 +1,60 @@
-import collections
+from os import listdir
 
-a = collections.deque()
-print(a)
+"""
+마크다운을 이용해 문제 풀이 결과를 적기 위한 코드입니다.
+- 디렉토리 내 파일 리스트를 가져옵니다.
+- 그리고 문항 번호를 슬라이싱 해서 웹에서 정보를 가져옵니다.
+- 마크다운 양식에 맞게 프린트 한 후 정리합니다.
+
+# Result
+|알고리즘|문항번호 + 문제 url|문제 이름|성공 여부|비고|
+|---|---|---|---|---|
+
+"""
+
+
+def get_parsing_file_list():
+    parsing_file_list = []
+
+    current_folder_file_names = listdir('.')
+    current_folder_file_names.remove('test.py')  # delete test.py file nmae in List
+
+    for file_name in current_folder_file_names:
+        if file_name[-2:] == 'py':
+            parsing_file_list.append(file_name)
+
+    return parsing_file_list
+
+
+def func(x):
+    return int(x.split('_', 1)[0])
+
+
+def sort_file_name_list(parsing_file_list):
+    sorted_file_name_list = sorted(parsing_file_list, key=func)
+
+    return sorted_file_name_list
+
+
+def print_markdown_form(sorted_file_name_list):
+    for i, file_name in enumerate(sorted_file_name_list):
+        problem_number = file_name.split('_', 1)[0]  # str.split([sep[, maxsplit]])
+        problem_title = file_name.split('_', 1)[1][:-3].replace(' ', '_')
+        try:
+            print(f'|{problem_number}|{problem_title}|[{problem_title}](https://github.com/timetobye/leetcode_solution/tree/master/solve/{file_name})'
+                  f'|성공| |')
+
+        except Exception as e:
+            """숫자 문자열이 아니면"""
+            pass
+
+
+def main():
+    parsing_file_list = get_parsing_file_list()
+    sorted_file_list = sort_file_name_list(parsing_file_list)
+
+    print_markdown_form(sorted_file_list)
+
+
+if __name__ == "__main__":
+    main()
